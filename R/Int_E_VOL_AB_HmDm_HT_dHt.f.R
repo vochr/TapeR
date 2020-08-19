b63f559175805d71a4e08aa2583f13d46425fd01
christian.vonderach <christian.vonderach@forst.bwl.de>
2020-08-19 09:26:34 +0200

    added roxygen meta data

--- R/Int_E_VOL_AB_HmDm_HT_dHt.f.R
+++ R/Int_E_VOL_AB_HmDm_HT_dHt.f.R
@@ -1,3 +1,30 @@
+#' @title Int_E_VOL_AB_HmDm_HT_dHt.f
+#'
+#' @param Hm Numeric vector of stem heights (m) along which diameter measurements 
+#' were taken for calibration. Can be of length 1. Must be of same length as \code{Dm}
+#' @param Dm Numeric vector of diameter measurements (cm) taken for calibration.
+#'  Can be of length 1. Must be of same length as \code{Hm}
+#' @param A Numeric scalar defining the lower threshold of a stem section for volume
+#' estimation. Depends on \code{iDH}. If \code{iDH} = "D", a diameter
+#' (cm), if \code{iDH} = "H", a height (m). If NULL, section starts at lowest point.
+#' @param B Numeric scalar defining the upper threshold of a stem section for volume
+#' estimation. Depends on \code{iDH}. If \code{iDH} = "D", a diameter
+#' (cm), if \code{iDH} = "H", a height (m). If NULL, section ends at tip.
+#' @param iDH Character scalar. Either "D" or "H". Type of threshold for
+#' section volume estimation. See \code{A} or \code{B}.
+#' @param mw_HtT Scalar. Tree height (m)
+#' @param sd_HtT Scalar. Standard deviation of stem height. Can be 0 if height was 
+#' measured without error
+#' @param par.lme List of taper model parameters obtained by \code{\link{TapeR_FIT_LME.f}} 
+#' @param IA Logic scalar. If TRUE, variance calculation of height
+#' estimate based on 2-point distribution. If FALSE, variance calculation of height
+#' estimate based on Normal approximation.
+#' @param nGL Numeric scalar. Number of support points for numerical integration.
+#' @param ... not currently used
+#'
+#' @return list with expected volume, variance of volume and squared expected value
+#' incorporating the uncertainty of height measurement
+
 Int_E_VOL_AB_HmDm_HT_dHt.f <-
 function(Hm, Dm, A = NULL, B = NULL, iDH = "D", mw_HtT, sd_HtT, par.lme, IA = F, nGL = 51, ...){
 #   ------------------------------------------------------------------------------------------------
