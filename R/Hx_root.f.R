672997c3d79115eb5645e83b9bd37292c91c6d7c
christian.vonderach <christian.vonderach@forst.bwl.de>
2020-08-19 07:28:20 +0200

    added roxygen meta data

--- R/Hx_root.f.R
+++ R/Hx_root.f.R
@@ -1,3 +1,23 @@
+#' @title find root (height) given diameter, measurements and fitted model
+#' @description Internal function not usually called by users
+#' @param Hx Numeric vector of stem heights (m) along which to return the 
+#' expected diameter
+#' @param Dx expected diameter
+#' @param Hm Numeric vector of stem heights (m) along which diameter measurements 
+#' were taken for calibration. Can be of length 1. Must be of same length as \code{Dm}
+#' @param Dm Numeric vector of diameter measurements (cm) taken for calibration.
+#'  Can be of length 1. Must be of same length as \code{Hm}
+#' @param mHt Scalar. Tree height (m)
+#' @param sHt Scalar. Standard deviation of stem height. Can be 0 if height was 
+#' measured without error
+#' @param par.lme List of taper model parameters obtained by 
+#' \code{\link{TapeR_FIT_LME.f}}.
+#' @param ... not currently used
+#' @details function is called by \code{\link[stats]{uniroot}} inside 
+#' \code{\link{E_HDx_HmDm_HT.f}}
+#' @return deviation between observed diameter \code{Dx} and diameter in height
+#' \code{Hx}.
+
 Hx_root.f <-
 function(Hx,Dx,Hm,Dm,mHt,sHt,par.lme, ...){
 #   ------------------------------------------------------------------------------------------------
