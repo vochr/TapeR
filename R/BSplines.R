2f9ef3737827c8f026318e5107510e2e31ae73f4
christian.vonderach <christian.vonderach@forst.bwl.de>
2020-10-19 17:34:14 +0200

    explicit return value; some code layout adjustments

--- R/BSplines.R
+++ R/BSplines.R
@@ -12,12 +12,13 @@
 #' @import splines
 
 BSplines <-
-function(knots=c(seq(0,1,0.1)), ord = 4, der = 0, x = c(seq(0,1,0.01)), ...){
+function(knots=c(seq(0, 1, 0.1)), ord = 4, der = 0, x = c(seq(0, 1, 0.01)), ...){
 
   # nur abhängig von TapeR-Modell und der relativen Messhöhe 'x'
 
 	TK = TransKnots(knots=knots, ord=ord) 
-	BSplines = splineDesign(knots=TK, x=x, ord = ord, derivs=c(rep(der,length(x))), outer.ok = T)
-
-  }
+	BSplines = splineDesign(knots=TK, x=x, ord=ord, derivs=c(rep(der,length(x))),
+	                        outer.ok=TRUE)
+  return(BSplines)
+}
 
